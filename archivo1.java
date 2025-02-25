ee32c41c52d2ed4c2022ab0253ad573b04dcddba
ibaiMa <ikimatarranz24@lhusurbil.eus>
2025-02-25 09:05:38 +0100

    archivo1.java

---
+++ archivo1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class EsPalindromo {
+    public static void main(String[] args) {
+        String texto; 
+        Scanner sc = new Scanner (System.in); 
+        System.out.println("Introduce un texto:"); 
+        int texto=sc.nextInt();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    
+}
