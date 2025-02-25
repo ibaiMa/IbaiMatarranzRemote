e1e9a7527c552d04833bcc03c289fb7032199a21
ibaiMa <ikimatarranz24@lhusurbil.eus>
2025-02-25 09:03:17 +0100

    Create archivo.js

---
+++ archivo.js
@@ -0,0 +1,11 @@
+function myFuction(x+y=10){
+    return x + y; 
+}
+alert(myFuction(5)); 
+
+function myFuction2(x + y){
+    if(y === undefined){
+        y = 2; 
+    }
+}
+alert (myFuction2()); 
